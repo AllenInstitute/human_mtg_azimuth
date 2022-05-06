eed7cee95fbe8eda4e3df137aec2da072ed5b15c
Rohan <rhngla@gmail.com>
2022-05-06 12:03:28 -0700

    map across donors

--- 03_metadata_to_feather.R
+++ 03_metadata_to_feather.R
@@ -5,20 +5,11 @@ df <- read.csv("~/Desktop/MTG_AD_metadata_full.2022-04-13.csv")
 keep_cols <-
     c(
         "sample_id",
-        "cell_name",
-        "cell_id",
-        "cellNames",
-        "n_genes",
-        "class",
-        "subclass",
-        "subclass_color",
-        "cluster",
-        "cluster_color",
-        "supertype",
         "subclass_scANVI",
         "supertype_scANVI",
         "for_analysis"
     )
 
-df_part <- df[keep_cols]
-write_feather(df_part, "~/Desktop/MTG_AD_metadata_2022-04-13.feather")
+df <- df[keep_cols]
+df <- df[df$for_analysis=="True", ]
+write_feather(df, "~/Desktop/MTG_AD_metadata_2022-04-13.feather")
